37faa8e19716ecad988e74a5795de592f35cc2c7
afirstenberg <github@addventure.com>
2018-03-20 18:18:20 -0400

    Use new standard handler configurations. Use new configuration objects. Use new response template.

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,26 @@
 const functions = require('firebase-functions');
+const Multivocal = require('multivocal');
 
-const Config = require('multivocal/lib/config-simple')({
+const Config = new Multivocal.Config.Simple({
   Local: {
     und: {
       hi: ['hello','hi','howdy','greetings'],
       Response: {
         Default: [
           {
-            Template: "Hello {{Val 'User/Name'}}.",
-            TemplateCard: {
-              title: "I bid you {{Oxford (Pick 3 Config.Local.und.hi)}}.",
-              formattedText: "This is your {{ordinalize User.State.NumVisits}} visit"+
+            Template: {
+              Text: "Hello {{Val 'User/Name'}}.",
+              Card: {
+                title: "I bid you {{Oxford (Pick 3 Config.Local.und.hi)}}.",
+                formattedText: "This is your {{ordinalize User.State.NumVisits}} visit"+
                 " and your {{ordinalize Session.State.NumVisits}} comment this round."
+              },
+              Suggestions: [
+                "{{#each (Val 'Config/Local/und/hi')}}",
+                "  {{Set '_This[+]' this}}",
+                "{{/each}}"
+              ]
             },
-            TemplateSuggestions: [
-              "{{#each (Val 'Config/Local/und/hi')}}",
-              "  {{Set '_This[+]' this}}",
-              "{{/each}}"
-            ],
             ShouldClose: false
           }
         ]
@@ -37,8 +40,6 @@ const Config = require('multivocal/lib/config-simple')({
   }
 });
 
-const Multivocal = require('multivocal');
-Multivocal.setConfig( Config );
 
 exports.webhook = functions.https.onRequest( (req,res) => {
     Multivocal.process( req, res );
